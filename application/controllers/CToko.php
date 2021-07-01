923e803f8fc676d30f0619eb35a14ee7e5a95825
Indiarto Aji Begawan <indizi130899@gmail.com>
2021-07-01 18:15:32 +0700

    Add HalBelanja

--- application/controllers/CToko.php
+++ application/controllers/CToko.php
@@ -1,9 +1,17 @@
 <?php
 class CToko extends CI_Controller
 {
+    
+
     function index()
     {
         $this->load->helper("url");
         $this->load->view("halutama");
+    }
+
+    function belanja()
+    {
+        $this->load->helper("url");
+        $this->load->view("halbelanja");
     }
 }
